f41746f7ed4b03d576d14879dbd0b44d581c5ddd
ethanknights <ethanknights@hotmail.co.uk>
2023-12-29 19:22:23 +0000

    add fetch and basic st / plotly plots

--- app.py
+++ app.py
@@ -3,12 +3,15 @@
 from dotenv import load_dotenv
 import streamlit as st
 import pandas as pd
-from utils import call_coinmarketcap_api
-from datetime import datetime
-import psycopg2
+from utils import (
+    create_psql_connection,
+    call_coinmarketcap_api,
+    process_coinmarketcap_api,
+    insert_psql_db_coinmarketcap,
+    fetch_data_from_db
+)
+import plotly.express as px
 
-
-# Load environment variables
 env = 'dev'
 dotenv_path = f'.env.{env}'
 load_dotenv(dotenv_path=dotenv_path)
@@ -16,45 +19,28 @@ load_dotenv(dotenv_path=dotenv_path)
 
 def main():
 
-    st.title('Market Prices')
+    # Request and write latest data from APIs
+    conn = create_psql_connection()
 
-    conn = psycopg2.connect(
-        dbname="postgres",
-        user="postgres",
-        password="mysecretpassword",
-        host="localhost",
-        port="5432"
-    )
-    cursor = conn.cursor()
+    # raw_coins = call_coinmarketcap_api()
+    # coins = process_coinmarketcap_api(raw_coins=raw_coins)
+    #
+    # insert_psql_db_coinmarketcap(conn_cursor=conn.cursor(), data=coins)
 
-    insert_query = """
-        INSERT INTO on_your_market_prices (coin_name, price, timestamp)
-        VALUES (%s, %s, %s);
-    """
+    df = fetch_data_from_db(conn)
 
-    # Example values
-    data_to_insert = ('Bitcoin', 50000, '2023-01-01 12:00:00')
-    cursor.execute(insert_query, data_to_insert)
-    conn.commit()
+    for coin_name in df['coin_name'].unique():
+        st.title(f"{coin_name} Price")
+        coin_data = df[df['coin_name'] == coin_name]
 
-    # get data
-    raw_coins = call_coinmarketcap_api()
+        # streamlit plot (inflexible)
+        # st.line_chart(data=coin_data, x='timestamp',y='price', use_container_width=True)
 
-    config_coins = {
-        'BTC_ID': int(1),
-        'ETH_ID': int(1027),
-        'BNB_ID': int(1839),
-        'SOL_ID': int(5426),
-    }
+        # plotly plot (more customisable)
+        fig = px.line(coin_data, x='timestamp', y='price', markers=True, line_shape='linear', title=f"{coin_name}")
+        st.plotly_chart(fig)
 
-    coins = [coin for coin in raw_coins['data'] if coin['id'] in config_coins.values()]
-    assert coins, 'No `coins` present after filter'
-
-    for coin in coins:
-        price = int(round(coin['quote']['GBP']['price']))  # rounding due to volatility
-        timestamp = datetime.strptime(coin['quote']['GBP']['last_updated'], "%Y-%m-%dT%H:%M:%S.%fZ")
-
-        print(f'{price} {timestamp}')
+    print('debug')
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -4,6 +4,19 @@ from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 import os
 import streamlit as st
+from datetime import datetime
+import psycopg2
+import pandas as pd
+
+
+def create_psql_connection():
+    return psycopg2.connect(
+        dbname="postgres",
+        user="postgres",
+        password="mysecretpassword",
+        host="localhost",
+        port="5432"
+    )
 
 def call_coinmarketcap_api():
     # base_url = 'https://sandbox-api.coinmarketcap.com'
@@ -31,3 +44,56 @@ def call_coinmarketcap_api():
         st.error(f"Error: {e}")
 
     return json_data
+
+
+def process_coinmarketcap_api(raw_coins):
+
+    config_coins = {
+        'BTC_ID': int(1),
+        'ETH_ID': int(1027),
+        'BNB_ID': int(1839),
+        'SOL_ID': int(5426),
+    }
+
+    coins = [coin for coin in raw_coins['data'] if coin['id'] in config_coins.values()]
+    assert coins, 'No `coins` present after filter'
+
+    return coins
+
+
+def insert_psql_db_coinmarketcap(conn_cursor, data):
+    insert_query = """
+        INSERT INTO on_your_market_prices (coin_name, price, timestamp)
+        VALUES (%s, %s, %s);
+    """
+
+    try:
+        for asset in data:
+            asset_name = asset['symbol']
+            price = int(round(asset['quote']['GBP']['price']))
+            timestamp = datetime.strptime(asset['quote']['GBP']['last_updated'], "%Y-%m-%dT%H:%M:%S.%fZ")
+
+            print(f'Ready for psql write: {price} {timestamp}')
+            data_to_insert = (asset_name, price, timestamp)
+            conn_cursor.execute(insert_query, data_to_insert)
+
+        conn_cursor.connection.commit()
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+    finally:
+        conn_cursor.close()
+
+    return
+
+
+def fetch_data_from_db(conn):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM on_your_market_prices")
+    data = cursor.fetchall()
+    cursor.close()
+
+    df = pd.DataFrame(data, columns=['id', 'coin_name', 'price', 'timestamp'])
+
+    return df
